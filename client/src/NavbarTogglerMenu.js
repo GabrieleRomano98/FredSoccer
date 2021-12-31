2498056ab8c5fc661a2016da3aa66f92ef185662
GabrieleRomano98 <gabrieleromanox@gmail.com>
2021-12-31 18:10:21 +0100

    make it better

--- client/src/NavbarTogglerMenu.js
+++ client/src/NavbarTogglerMenu.js
@@ -16,7 +16,7 @@ function NavbarTogglerMenu(props) {
                                 <NavDropdown.Item onClick={props.doLogOut} style={{color: "#97fb57"}}><Link>Logout</Link></NavDropdown.Item>
                             :<>
                                 <NavDropdown.Item><Link style={{color: "#97fb57"}} to="Login">Login</Link></NavDropdown.Item>
-                                <NavDropdown.Item href="SignUp"><Link style={{color: "#97fb57"}} to="SignUp">SignUp</Link></NavDropdown.Item>
+                                <NavDropdown.Item><Link style={{color: "#97fb57"}} to="SignUp">SignUp</Link></NavDropdown.Item>
                             </>}
                             {props.logged && <NavDropdown.Item><Link to="AreaRiservata" style={{color: "#97fb57"}}>Area Riservata</Link></NavDropdown.Item>}
                             <NavDropdown.Divider />
